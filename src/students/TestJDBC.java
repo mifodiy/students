d0fe5aebd6d82ca5ffb003013a6b7b43e422eeb3
mifodiy911 <mifodiy911@gmail.com>
2020-06-03 23:23:42 +0300

    Connection to the Mysql server

---
+++ src/students/TestJDBC.java
@@ -0,0 +1,53 @@
+package students;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.sql.*;
+
+import static students.logic.ManagementSystem.printString;
+
+public class TestJDBC
+{
+    public static void main(String[] args) {
+        try{
+            System.setOut(new PrintStream("out.txt"));
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+            return;
+        }
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/students";
+            con = DriverManager.getConnection(url, "root", "root");
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM students");
+            while (rs.next()){
+                String str = rs.getString(1) + " : " + rs.getString(2);
+                printString(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (rs != null){
+                    rs.close();
+                }
+                if (stmt != null){
+                    stmt.close();
+                }
+                if (con != null){
+                    con.close();
+                }
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+
+    }
+}
